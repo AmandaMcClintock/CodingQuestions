e7d4fa28620bf59e3bbf933b2b21ca2c0afaaa3b
Amanda McClintock <amcclintock@mac-amcclintock1.cb.careerbuilder.com>
2017-12-06 15:38:33 -0500

    Added new Coding Sample

---
+++ DataFinch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DataFinch
+{
+    class Program
+    {
+            //TODO: Return array of duplicates
+        static void GetDuplicates(int[] a)
+        {
+            Dictionary<int, int> DuplicateKeeper = new Dictionary<int, int>();
+            int ArrayLen = a.Length;
+            for(int loop = 0; loop < ArrayLen; loop++){
+                //Console.WriteLine(a[loop]);
+                if (!DuplicateKeeper.ContainsKey(a[loop]))
+                {
+                    DuplicateKeeper.Add(a[loop], 1);
+                }
+                else
+                {
+                    DuplicateKeeper[a[loop]]++;
+                }
+            }
+            //TODO: Every number in the array is added to the Dictionary. Fix it.
+            for(int l = 0; l < ArrayLen; l++)
+            {
+                // if(DuplicateKeeper[a[l]] < 0)
+                // {
+                //     Console.WriteLine(a[l]);
+                // }
+                Console.WriteLine(DuplicateKeeper[a[l]]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int[] a = {3,5,5,8,9,9,9};
+            GetDuplicates(a);
+        }
+    }
+}
